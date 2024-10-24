1b0e1cd7e1f01fff5db5d75089c3d94df574e272
Jace Babin <jbabin91@gmail.com>
2024-10-24 11:14:06 -0700

    initial commit
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,141 @@
+import js from '@eslint/js';
+import query from '@tanstack/eslint-plugin-query';
+import router from '@tanstack/eslint-plugin-router';
+import configPrettier from 'eslint-config-prettier';
+import importX from 'eslint-plugin-import-x';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import sortKeysFix from 'eslint-plugin-sort-keys-fix';
+import tailwindcss from 'eslint-plugin-tailwindcss';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist', '**/*.gen.ts'] },
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+  ...query.configs['flat/recommended'],
+  ...router.configs['flat/recommended'],
+  {
+    extends: [js.configs.recommended],
+    plugins: {
+      'import-x': importX,
+      'simple-import-sort': simpleImportSort,
+      'sort-keys-fix': sortKeysFix,
+      unicorn,
+    },
+    rules: {
+      ...unicorn.configs['flat/recommended'].rules,
+      'import-x/first': 'error',
+      'import-x/newline-after-import': 'error',
+      'import-x/no-cycle': 'error',
+      'import-x/no-duplicates': ['error', { 'prefer-inline': true }],
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+      'sort-keys-fix/sort-keys-fix': 'error',
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            kebabCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+      'unicorn/no-null': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
+  {
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ['**/*.ts?(x)'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { fixStyle: 'inline-type-imports', prefer: 'type-imports' },
+      ],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/only-throw-error': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+    },
+  },
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'jsx-a11y': jsxA11y,
+      // @ts-ignore
+      react,
+      'react-hooks': reactHooks,
+      tailwindcss,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
+      ...tailwindcss.configs.recommended.rules,
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          ignoreCase: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+          shorthandFirst: true,
+          shorthandLast: false,
+        },
+      ],
+      'react/no-unknown-property': 'off',
+      'react/prop-types': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  configPrettier,
+);
